90f50450d3b8644d5c09d2f1e114f19f61b6e373
Admin <Admin@DESKTOP-92ROU2U.bbrouter>
2023-05-06 23:22:19 -0400

    Execute test with multiple inputs

--- src/test/java/script/InValidLogin.java
+++ src/test/java/script/InValidLogin.java
@@ -9,28 +9,28 @@ import page.LoginPage;
 
 public class InValidLogin extends BaseTest
 {
-    @Test(priority=2)
-    public void testInvalidLogin()
-    {
-    
-     int rc = Excel.getRowCount(XL_PATH,"InvalidLogin");
-     for(int i=1;i<=rc;i++)
-     {
-     
-    	String un = Excel.getData(XL_PATH, "InvalidLogin", 1, 0);
-        String pw = Excel.getData(XL_PATH, "InvalidLogin", 1, 1);
-    	
-    	
-      //1:Enter Invalid UN
-      LoginPage loginPage = new LoginPage(driver);
-      loginPage.setUserName(un);
-      //2.Enter Invalid PW
-      loginPage.setPassword(pw);
-      //3.Click on Login Button
-      loginPage.clickLoginButton();
-      //Err Msg Should Be Displayed
-      boolean result = loginPage.verifyErrMsgIsDisplayed(wait);
-      Assert.assertTrue(result);
-    }
-    }
+	@Test(priority=2)
+	public void testInvalidLogin()
+	{
+
+		int rc = Excel.getRowCount(XL_PATH,"InvalidLogin");
+		for(int i=1;i<=rc;i++)
+		{
+
+			String un = Excel.getData(XL_PATH, "InvalidLogin", i, 0);
+			String pw = Excel.getData(XL_PATH, "InvalidLogin", i, 1);
+
+
+			//1:Enter Invalid UN
+			LoginPage loginPage = new LoginPage(driver);
+			loginPage.setUserName(un);
+			//2.Enter Invalid PW
+			loginPage.setPassword(pw);
+			//3.Click on Login Button
+			loginPage.clickLoginButton();
+			//4:Err Msg Should Be Displayed
+			boolean result = loginPage.verifyErrMsgIsDisplayed(wait);
+			Assert.assertTrue(result);
+		}
+	}
 }
